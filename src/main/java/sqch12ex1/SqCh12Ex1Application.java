ca70dd5e90856eb2f0bb8bd9035c1d77249349cf
osparking <jbpark03@gmail.com>
2023-10-28 07:17:55 +0900

    최초판 - 종속: 웹 및 롬복

---
+++ src/main/java/sqch12ex1/SqCh12Ex1Application.java
@@ -0,0 +1,13 @@
+package sqch12ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh12Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh12Ex1Application.class, args);
+	}
+
+}
