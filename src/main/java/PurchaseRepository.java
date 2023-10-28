f542466c9d681ab53f82e49f0dd51e3d7e504286
osparking <jbpark03@gmail.com>
2023-10-29 07:22:54 +0900

    DB 사용을 위한 종속성(jdbcTemplate) 주입

--- src/main/java/PurchaseRepository.java
+++ src/main/java/PurchaseRepository.java
@@ -1,6 +1,12 @@
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import lombok.AllArgsConstructor;
+
 @Repository
+@AllArgsConstructor
 public class PurchaseRepository {
+	
+	private final JdbcTemplate jdbcTemplate;
 
 }
