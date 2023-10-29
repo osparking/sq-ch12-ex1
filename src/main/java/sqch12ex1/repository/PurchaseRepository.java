b0c71eff999c0f99f2565011b571b3fa468f9bb8
osparking <jbpark03@gmail.com>
2023-10-29 15:25:49 +0900

    저장소 클래스 패키지 변화

--- src/main/java/sqch12ex1/repository/PurchaseRepository.java
+++ src/main/java/sqch12ex1/repository/PurchaseRepository.java
@@ -1,5 +1,8 @@
 package sqch12ex1.repository;
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import lombok.AllArgsConstructor;
@@ -8,11 +11,26 @@ import sqch12ex1.model.Purchase;
 @Repository
 @AllArgsConstructor
 public class PurchaseRepository {
-	
+
 	private final JdbcTemplate jdbcTemplate;
 
 	public void storePurchase(Purchase purchase) {
 		jdbcTemplate.update("insert into purchase(null, ?, ?)",
-				purchase.get)
+				purchase.getProduct(), purchase.getPrice());
+	}
+
+	public List<Purchase> findAllPurchases() {
+		String sql = "SELECT * FROM purchase";
+
+		RowMapper<Purchase> purchaseMapper = (r, i) -> {
+			var purchase = new Purchase();
+			purchase.setId(r.getInt("id"));
+			purchase.setProduct(r.getString("product"));
+			purchase.setPrice(r.getBigDecimal("price"));
+			System.out.println("행 번호: " + i);
+			return purchase;
+		};
+
+		return jdbcTemplate.query(sql, purchaseMapper);
 	}
 }
