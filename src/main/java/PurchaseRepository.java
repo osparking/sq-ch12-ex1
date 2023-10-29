ffa556c93c0f181f122630171f1ebe74d219001f
osparking <jbpark03@gmail.com>
2023-10-29 10:42:43 +0900

    Purchase 테이블의 모든 행을 읽는 저장소 메소드

--- src/main/java/PurchaseRepository.java
+++ src/main/java/PurchaseRepository.java
@@ -1,4 +1,7 @@
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import lombok.AllArgsConstructor;
@@ -7,11 +10,25 @@ import sqch12ex1.model.Purchase;
 @Repository
 @AllArgsConstructor
 public class PurchaseRepository {
-	
+
 	private final JdbcTemplate jdbcTemplate;
 
 	public void storePurchase(Purchase purchase) {
 		jdbcTemplate.update("insert into purchase(null, ?, ?)",
 				purchase.getProduct(), purchase.getPrice());
+	}
+
+	public List<Purchase> findAllPurchases() {
+		String sql = "SELECT * FROM purchase";
+
+		RowMapper<Purchase> purchaseMapper = (r, i) -> {
+			var purchase = new Purchase();
+			purchase.setId(r.getInt("id"));
+			purchase.setProduct(r.getString("product"));
+			purchase.setPrice(r.getBigDecimal("price"));
+			return purchase;
+		};
+
+		return jdbcTemplate.query(sql, purchaseMapper);
 	}
 }
