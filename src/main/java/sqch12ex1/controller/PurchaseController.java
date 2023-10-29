8051d82964a4eb9ce8fdb1f5f3e4cf2cbf332a66
osparking <jbpark03@gmail.com>
2023-10-29 15:29:39 +0900

    저장소 참조변수에 자동 주입하기

--- src/main/java/sqch12ex1/controller/PurchaseController.java
+++ src/main/java/sqch12ex1/controller/PurchaseController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import sqch12ex1.model.Purchase;
 import sqch12ex1.repository.PurchaseRepository;
 
 @RestController
 @RequestMapping("/purchase")
+@AllArgsConstructor
 public class PurchaseController {
 	
 	private PurchaseRepository repository;
